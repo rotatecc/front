5cf75b972673fcdf2c99f9ef4d484d96723e0be3
Andrew Suzuki <andrew.b.suzuki@gmail.com>
2017-04-18 00:39:51 -0400

    Base styles for Row, Column

--- src/bearings/components/Column/index.js
+++ src/bearings/components/Column/index.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import { styled } from 'styletron-react'
 
-export default function Column({ children }) {
-  return <div>{children}</div>
+import { expandStyles } from '../../utils'
+
+
+const StyledDiv = styled('div', expandStyles(
+  'd/block',
+  'fBasis/0',
+  'fGrow/1',
+  'fShrink/1',
+  'p/0.75rem',
+))
+
+
+export default function Column({ children, ...restProps }) {
+  return React.createElement(StyledDiv, restProps, children)
 }
 
 Column.propTypes = {
--- src/bearings/components/Row/index.js
+++ src/bearings/components/Row/index.js
@@ -1,11 +1,33 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import { styled } from 'styletron-react'
 
-export default function Row({ children }) {
-  return <div>{children}</div>
+import { expandStyles } from '../../utils'
+
+
+const StyledDiv = styled('div', expandStyles(
+  'd/flex',
+  'mLeft/-0.75rem',
+  'mRight/-0.75rem',
+  'mTop/-0.75rem',
+))
+
+
+export default class Row extends React.PureComponent {
+  getChildContext() {
+    return {}
+  }
+
+  render() {
+    const { children, ...restProps } = this.props
+    return React.createElement(StyledDiv, restProps, children)
+  }
 }
 
 Row.propTypes = {
   children: PropTypes.node,
+}
+
+Row.childContextTypes = {
 }
